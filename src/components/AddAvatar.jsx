e34d78c4b0a39ae862db1833fe9e0d590c901dae
rebehayan <rebehayan@gmail.com>
2024-05-14 11:35:36 +0900

    Fix:아바타변경

---
+++ src/components/AddAvatar.jsx
@@ -0,0 +1,12 @@
+const AddAvatar = ({ value, onChange, src }) => {
+  return (
+    <>
+      <label htmlFor="avatar" className="block border-slate-300 border-[1px] w-full aspect-square bg-white cursor-pointer">
+        <img src={src} alt="" />
+      </label>
+      <input type="file" onChange={onChange} value={value} name="" id="avatar" className="sr-only" />
+    </>
+  );
+};
+
+export default AddAvatar;
